8cf21b2e5f8647196ad06871a50eadf9486ec3ad
SaintRayen <rmsyal@avans.nl>
2019-04-14 20:08:15 +0200

    Main Updated
    Main class is initilializing the Connection AND the GUI

--- src/main.java
+++ src/main.java
@@ -1,10 +1,16 @@
 import DataStorageLayer.DatabaseConnection;
+import PresentationLayer.GraphicalUserInterface;
+
+import javax.swing.*;
 
 public class main {
 
     public static void main(String[] args) {
+        GraphicalUserInterface graphicalUserInterface = new GraphicalUserInterface();
+
         // Initializing the database connection
         DatabaseConnection.connect();
+        SwingUtilities.invokeLater(graphicalUserInterface);
     }
 }
 
